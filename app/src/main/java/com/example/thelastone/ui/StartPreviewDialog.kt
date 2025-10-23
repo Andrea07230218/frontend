d28bc5d0c1598e3ad7e52f554d14fa6df3103336
Andrea07230218 <tsailinwang218@gmail.com>
2025-10-23 13:41:20 +0800

    test

--- app/src/main/java/com/example/thelastone/ui/StartPreviewDialog.kt
+++ app/src/main/java/com/example/thelastone/ui/StartPreviewDialog.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.thelastone.data.model.StartInfo
 
+//test
 @Composable
 fun StartPreviewDialog(
     info: StartInfo,
